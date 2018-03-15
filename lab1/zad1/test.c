262f33da4ea29987b2cabf60bec0f5e7e8515dc6
Shino1024 <justincompspec@gmail.com>
2018-03-15 01:12:24 +0100

    Tidying up.

--- lab1/zad1/test.c
+++ lab1/zad1/test.c
@@ -1,42 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "hdr/typedefs.h"
-
-#include "hdr/array_dynamic.h"
-#include "hdr/array_static.h"
-
-// included only for testing
-//#include "imp/array_static.c"
-
-//#include "imp/array_dynamic.c"
-
-//#include "imp/util.c"
+#include "condensed_lib.c"
 
 int main(int argc, char *argv[]) {
-	append_block_static(4);
-	append_block_static(5);
-	append_block_static(6);
+	make_static_array(555, 555);
+	append_block_gen_static(4);
+	zero_out_static_array();
+	make_static_array(555, 555);
+	append_block_gen_static(5);
+	append_block_gen_static(6);
 
 	pop_block_static(4);
 	pop_block_static(7);
 	pop_block_static(9);
-	pop_block_static(5);
+	//pop_block_static(5);
 	pop_block_static(7);
 	pop_block_static(8);
 	pop_block_static(4);
 	
-	append_block_static(4);
-append_block_static(6);
-append_block_static(5);
-append_block_static(8);
-append_block_static(9);
-append_block_static(11);
-append_block_static(11111);
-append_block_static(4);
-append_block_static(0);
+	append_block_gen_static(4);
+append_block_gen_static(6);
+append_block_gen_static(5);
+append_block_gen_static(8);
+append_block_gen_static(9);
+append_block_gen_static(11);
+append_block_gen_static(11111);
+append_block_gen_static(4);
+append_block_gen_static(0);
 
-zero_out_static_array();
+//zero_out_static_array();
 
 
 	printf("after\n");
@@ -46,7 +39,7 @@ zero_out_static_array();
 	array_dynamic *ad = make_array_dynamic(120, 150);
 	unsigned int i;
 	for (i = 2; i < ad->array_size + 8; i += 3) {
-		append_block_dynamic(ad, i);
+		append_block_gen_dynamic(ad, i);
 	}
 
 	for (i = 3; i < ad->array_size + 11; i += 2) {
