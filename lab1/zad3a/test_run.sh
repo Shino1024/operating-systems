a3c70a2fa293f68d2691128c80d60247ed420ee0
Shino1024 <justincompspec@gmail.com>
2018-03-16 00:31:23 +0100

    Still.

---
+++ lab1/zad3a/test_run.sh
@@ -0,0 +1,6 @@
+./program_static -a 50 -b 80 -f 20 -x 30 -y 30 > raport3a.txt
+./program_static -a 50 -b 80 -f 20 -x 30 -y 30 -s >> raport3a.txt
+./program_shared -a 50 -b 80 -f 20 -x 30 -y 30 >> raport3a.txt
+./program_shared -a 50 -b 80 -f 20 -x 30 -y 30 -s >> raport3a.txt
+./program_dynamic -a 50 -b 80 -f 20 -x 30 -y 30 >> raport3a.txt
+./program_dynamic -a 50 -b 80 -f 20 -x 30 -y 30 -s >> raport3a.txt
