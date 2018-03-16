890ddbd03020c5e912745916dcaacb7fdfbde14a
Shino1024 <justincompspec@gmail.com>
2018-03-16 16:31:17 +0100

    Fixes.

--- lab1/zad3a/test_run.sh
+++ lab1/zad3a/test_run.sh
@@ -1,6 +1,7 @@
-./program_static -a 50 -b 80 -f 20 -x 30 -y 30 > raport3a.txt
-./program_static -a 50 -b 80 -f 20 -x 30 -y 30 -s >> raport3a.txt
-./program_shared -a 50 -b 80 -f 20 -x 30 -y 30 >> raport3a.txt
-./program_shared -a 50 -b 80 -f 20 -x 30 -y 30 -s >> raport3a.txt
-./program_dynamic -a 50 -b 80 -f 20 -x 30 -y 30 >> raport3a.txt
-./program_dynamic -a 50 -b 80 -f 20 -x 30 -y 30 -s >> raport3a.txt
+RES=raport3a.txt
+./program_static -a 50 -b 80 -f 20 -x 30 -y 30 >> ${RES}
+./program_static -a 50 -b 80 -f 20 -x 30 -y 30 -s >> ${RES}
+./program_shared -a 50 -b 80 -f 20 -x 30 -y 30 >> ${RES}
+./program_shared -a 50 -b 80 -f 20 -x 30 -y 30 -s >> ${RES}
+./program_dynamic -a 50 -b 80 -f 20 -x 30 -y 30 >> ${RES}
+./program_dynamic -a 50 -b 80 -f 20 -x 30 -y 30 -s >> ${RES}
